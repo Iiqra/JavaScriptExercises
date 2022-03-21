0ec95bedcc5239421a87a1fd07ba39be375e3e66
sahaya-annie <sahaya@leaf.fm>
2022-03-21 18:03:29 +0000

    Added new files to review

--- Practice Exercises/operator.js
+++ Practice Exercises/operator.js
@@ -14,9 +14,22 @@ console.log(x*y)
 console.log(y/x)
 console.log(x/y)
 console.log(x ** y)
+console.log(x += y)
+console.log(x -= y)
+console.log(x *= y)
+console.log(x /= y)
+console.log(x %= y)
+console.log(x **= y)
+
 
 //incemental/decremental
 let p =1
 let q = p++
 console.log(p++)
 console.log(q)
+
+//Adding Strings and Numbers
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+console.log(z)
