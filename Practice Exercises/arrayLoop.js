740c33908f40ee53150bf4a882e677bfc2679504
sahaya-annie <sahaya@leaf.fm>
2022-03-31 00:42:40 +0100

    Added Array files for review

--- Practice Exercises/arrayLoop.js
+++ Practice Exercises/arrayLoop.js
@@ -3,6 +3,8 @@ let student1Courses = ['Math', 'English', 'Programming', 'Science'];
 let student2Courses = ['Geography', 'Spanish', 'Programming', 'Math'];
 
 let commonCourse = [...student1Courses, ...student2Courses].reduce((total, item, index, array) => {
+  console.log(item,index);
+  console.log("total",total,array.indexOf(item, index + 1));
   (array.indexOf(item, index + 1) !== -1 && total.indexOf(item) === -1) ? total.push(item) : null
   return total
 }, [])
