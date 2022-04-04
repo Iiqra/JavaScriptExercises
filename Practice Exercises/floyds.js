c962555c789dc76235dc7c8aa33f4b6b7aa5e39e
sahaya-annie <sahaya@leaf.fm>
2022-04-04 21:01:37 +0100

    Added exercises on Math functions & Promises

--- Practice Exercises/floyds.js
+++ Practice Exercises/floyds.js
@@ -1,11 +1,27 @@
 function floyds(){
   for(let i=0;i<=4;i++)
 {
+  star = '';
   for(let j= 4;j>=i;j--)
    {
-    console.log('*')
+    star += '*'
     }
-    console.log("\n")
+    console.log(star)
 }
 }
 floyds()
+
+function nestedStairs(step) {
+	var stairs = "";
+
+	for (var i = 0; i < step; i++) 
+  {
+		for (var j = 0; j <= i; j++)
+     {
+			stairs += " * ";
+		}
+		console.log(stairs)
+  }
+}
+
+nestedStairs(10);
